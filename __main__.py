1e254d076161a83f65ed2604525068c4917268e0
Maksim Andreev <andrianov_maksim@outlook.com>
2024-12-01 22:30:01 +0300

    mypy found 2 errors, code is good

--- __main__.py
+++ __main__.py
@@ -1,6 +1,4 @@
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
-
+from src.masks import get_mask_account, get_mask_card_number
 
 if __name__ == '__main__':
     print(get_mask_card_number(1234567891234567))
