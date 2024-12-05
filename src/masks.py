a4fb83640061a21aabcdc62e524e3836ffb30fc7
Maksim Andreev <andrianov_maksim@outlook.com>
2024-12-06 01:25:13 +0300

    I have added the basic logic to the masks

--- src/masks.py
+++ src/masks.py
@@ -1,32 +1,30 @@
 # Functional part
-def get_mask_card_number(card_number: [int] = None) -> [str]:
-    """принимает на вход номер карты в виде числа и возвращает маску номера по правилу XXXX XX** **** XXXX"""
-    number_to_string = str(card_number)
-    out_format = ""
-    split = 3
+def get_mask_card_number(card_number: [str] = None, start=0) -> [str]:
+    """принимает на вход номер карты, индекс первой цыфры номера карты и возвращает маску номера 
+       по правилу User Name XXXX XX** **** XXXX"""
+    out_format = card_number[:start]
+    split = start + 4
+    temp = range(start+7, start+13)
 
-    for i in range(len(number_to_string)):
-        if i in [6, 7, 8, 9, 10, 11]:
-            out_format += "*"
-            if i == split:
-                out_format += " "
-                split += 4
-        else:
-            out_format += number_to_string[i]
-            if i == split:
-                out_format += " "
-                split += 4
+    for i in range(len(card_number)):
+        if i > start:
+            if i in temp:
+                out_format += "*"
+                if i == split:
+                    out_format += " "
+                    split += 4
+            else:
+                out_format += card_number[i]
+                if i == split:
+                    out_format += " "
+                    split += 4
     return out_format
 
 
-def get_mask_account(bank_account: [int] = None) -> [str]:
-    """принимает на вход номер счета в виде числа и возвращает маску номера по правилу **XXXX"""
-    number_to_string = str(bank_account)
+def get_mask_account(bank_account: [str] = None, start=0) -> [str]:
+    """принимает на вход номер счета и возвращает маску номера по правилу Name **XXXX"""
     out_format = ""
 
-    for i in range(len(number_to_string)):
-        if i in [0, 1]:
-            out_format += "*"
-        else:
-            out_format += number_to_string[i]
+    out_format += bank_account[:start] + "**" + bank_account[-4:]
+
     return out_format
