db9531a93bec2f54846e0cf41b3bdc42cd2a2e83
Maksim Andreev <andrianov_maksim@outlook.com>
2024-12-17 14:26:14 +0300

    function body sort by date and add tests

--- __main__.py
+++ __main__.py
@@ -2,4 +2,12 @@ import src.processing
 import src.widget
 
 if __name__ == '__main__':
-    print(src.processing.filter_by_state())
+    test = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, 
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, 
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, 
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+
+    # print(src.processing.filter_by_state(test))
+    print(src.processing.sort_products_by_quantity(test))
