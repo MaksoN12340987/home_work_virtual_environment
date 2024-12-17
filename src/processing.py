33bfec94445fd1db9d9d6ed831f94dc358099a70
Maksim Andreev <andrianov_maksim@outlook.com>
2024-12-17 15:03:04 +0300

    I finished writing the function bodysort_by_date, the test is ok

--- src/processing.py
+++ src/processing.py
@@ -10,12 +10,8 @@ def filter_by_state(to_sort: list = [], state: str='EXECUTED') -> list:
 
 
 # Sort list by date
-def sort_products_by_quantity(operation_list: list = [], date: bool=False) -> list:
-    '''Принимает список словарей и параметр сортировки (по умолчанию — убывание). 
+def sort_products_by_quantity(operation_list: list = [], date: bool=True) -> list:
+    '''Принимает список словарей и параметр сортировки (по умолчанию "True" — убывание). 
        Функция возвращаtn новый список, отсортированный по дате (date)'''
-    for i in range(len(operation_list)):
-        if not operation_list[i].get("quantity", False):
-            item = operation_list.pop(ist_product[i])
-    operation_list.sort(key=lambda operation_list: operation_list[date], reverse=ascending)
-    return ist_product
-
+    operation_list.sort(key=lambda operation_list: operation_list["date"][:9], reverse=date)
+    return operation_list
