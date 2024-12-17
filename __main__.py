4f491fb26bf62271f83fa147c716d39f29883d9c
Maksim Andreev <andrianov_maksim@outlook.com>
2024-12-17 13:58:46 +0300

    filter_by_state added to main, main file checked, ok

--- __main__.py
+++ __main__.py
@@ -1,8 +1,5 @@
+import src.processing
 import src.widget
 
 if __name__ == '__main__':
-    test_1 = "Visa Platinum 7000792289606361"
-    test_2 = "Счет 73654108430135874305"
-
-    print(src.widget.mask_account_card(test_1))
-    print(src.widget.mask_account_card(test_2))
+    print(src.processing.filter_by_state())
