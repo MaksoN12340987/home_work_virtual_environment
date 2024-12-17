d68d01d1c3e3163ea9b3477e679fad722518201d
Maksim Andreev <andrianov_maksim@outlook.com>
2024-12-17 18:30:18 +0300

    fixed to pull request

--- __main__.py
+++ __main__.py
@@ -2,7 +2,13 @@ import src.processing
 import src.widget
 
 if __name__ == "__main__":
-    test = [
+    test_1 = "Visa Platinum 7000792289606361"
+    test_2 = "Счет 73654108430135874305"
+    
+    print(src.widget.mask_account_card(test_1))
+    print(src.widget.mask_account_card(test_2))
+
+    test_3 = [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
@@ -10,5 +16,5 @@ if __name__ == "__main__":
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-13T08:20:00.419441"}
     ]
 
-    # print(src.processing.filter_by_state(test, "CANCELED"))
-    print(src.processing.sort_by_date(test, False))
+    print(src.processing.filter_by_state(test_3, "CANCELED"))
+    print(src.processing.sort_by_date(test_3, False))
