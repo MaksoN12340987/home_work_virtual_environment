e563692e31dd5f2e9389b0d34b628adf3a72bc3c
Maksim Andreev <andrianov_maksim@outlook.com>
2024-12-17 13:22:53 +0300

    he function is finished, not tested

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,24 @@
 # Bank transactions sorting function
-def filter_by_state(to_sort: list = [], state='EXECUTED') -> list:
-    pass
+def filter_by_state(to_sort: list = [], state: str='EXECUTED') -> list:
+    finaly_list = []
+    for i in range(len(to_sort)):
+        if to_sort[i].get(state, "") != "":
+            finaly_list.append(to_sort[i])
+    return finaly_list
+
+products = [
+    {"name": "Apple", "category": "fruit", "price": 120, "quantity": 10},
+    {"name": "Banana", "category": "fruit", "price": 90, "quantity": 15},
+    {"name": "Avocado", "category": "fruit", "price": 200, "quantity": 5},
+    {"name": "Tomato", "category": "veggie", 'EXECUTED': 100, "quantity": 20},
+    {"name": "Broccoli", "category": "veggie", "price": 300, "quantity": 8},
+    {"name": "Carrot", "category": "veggie", "price": 100},
+    {"name": "Cookie", "category": "sweets", "price": 200, "quantity": 12, "brand": "ABC"},
+    {"name": "Donut", "category": "sweets", "price": 300, "quantity": 7, "brand": "XYZ"},
+    {"name": "Cake", "category": "sweets", "price": 400, "quantity": 3, "brand": "DEF", "discount": 10},
+    {"name": "Orange", "category": "fruit", "price": 150, "quantity": 18},
+    {"name": "Lettuce", "category": "veggie", "price": 80, "quantity": 30, "organic": True},
+    {"name": "Chocolate", "category": "sweets", "price": 250, "quantity": 10, "brand": "GHI", "flavor": "Dark"}
+]
+
+print(filter_by_state(products, "quantity"))
